aa63c56282526360c21b9632d01bc6c85d7e9f46
= <emmanuelgatwech@gmail.com>
2026-01-07 22:47:56 +0300

    chore: test new deployment target

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
   plugins: [reactRouter(), tsconfigPaths(), netlifyReactRouter(), netlify()],
 });
+
+
+// import { reactRouter } from "@react-router/dev/vite";
+// import { defineConfig } from "vite";
+// import tsconfigPaths from "vite-tsconfig-paths";
+// import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+
+// export default defineConfig({
+//   plugins: [
+//     reactRouter(),
+//     tsconfigPaths(),
+//     netlifyReactRouter()
+//   ]
+// });
+
+// import { reactRouter } from "@react-router/dev/vite";
+// import autoprefixer from "autoprefixer";
+// import tailwindcss from "tailwindcss";
+// import { defineConfig } from "vite";
+// import tsconfigPaths from "vite-tsconfig-paths";
+
+// export default defineConfig({
+//   plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+// });
