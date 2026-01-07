73641c3bb8f5ddcc12eba5460fc5c026d00f49d6
= <emmanuelgatwech@gmail.com>
2026-01-07 22:53:40 +0300

    chore: update start command

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router";
-import netlify from "@netlify/vite-plugin";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), netlifyReactRouter(), netlify()],
+  plugins: [reactRouter(), tailwindcss(), tsconfigPaths(), netlifyPlugin()],
 });
 
 
