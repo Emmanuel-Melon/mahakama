679a8ee1222f34f20bfcca5452c4807f85a6342e
= <emmanuelgatwech@gmail.com>
2026-01-07 22:59:06 +0300

    chore: clean presets

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
-
+// ↓ add this
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [
+    reactRouter(),
+    tailwindcss(),
+    tsconfigPaths(),
+    // netlifyReactRouter() // ← add this
+  ]
 });
+// import { reactRouter } from "@react-router/dev/vite";
+
+// import { defineConfig } from "vite";
+// import tsconfigPaths from "vite-tsconfig-paths";
+// import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
+// export default defineConfig({
+//   plugins: [reactRouter(), tailwindcss(), tsconfigPaths(), netlifyPlugin()],
+// });
 
 
 // import { reactRouter } from "@react-router/dev/vite";
